944284ae4d515c02b53fd326da4bb9abdf615fd8
Samuel <samycsc4@gmail.com>
2025-08-08 14:00:00 +0200

    Fixed file extensions and implemented exclusion tax algorithm

---
+++ vidigal_optimizer.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+class InformalEconomyOptimizer:
+    def __init__(self, daily_capacity=15, base_price_brl=80):
+        self.capacity = daily_capacity
+        self.base_price = base_price_brl
+        self.analog_no_show_rate = 0.30  
+        self.digital_no_show_rate = 0.08 
+
+    def load_data(self, filepath):
+        self.df = pd.read_csv(filepath)
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        return self.df.head()
+
+    def calculate_digital_exclusion_tax(self, days_active=30):
+        """
+        Quantifies the revenue lost due to lack of digital visibility.
+        """
+        # Scenario A: Analog
+        analog_occupancy = 0.41 
+        analog_revenue = (self.capacity * analog_occupancy * self.base_price * days_active) * (1 - self.analog_no_show_rate)
+
+        # Scenario B: Digital Integration
+        digital_occupancy = 0.60 
+        digital_price_premium = 1.15 
+        digital_revenue = (self.capacity * digital_occupancy * (self.base_price * digital_price_premium) * days_active) * (1 - self.digital_no_show_rate)
+
+        exclusion_tax = digital_revenue - analog_revenue
+        return exclusion_tax
